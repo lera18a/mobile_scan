3dea558fc97688c9e17387ec3651392c8920eba8
Badrakova Valeria <lera_18a@mail.ru>
2026-02-04 21:03:14 +0300

    feat(injectors): add bloc injector

---
+++ lib/widget/injectors/bloc_injector.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile_scan/abstract/product_repository.dart';
+import 'package:mobile_scan/abstract/scanner.dart';
+import 'package:mobile_scan/bloc/bloc/barcode_bloc.dart';
+
+class BlocInjector extends StatelessWidget {
+  const BlocInjector({super.key, required this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => BarcodeBloc(
+        context.read<Scanner>(),
+        context.read<ProductRepository>(),
+      ),
+      child: child,
+    );
+  }
+}

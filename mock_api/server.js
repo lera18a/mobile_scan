d4091f0b250273c73be54283a095c15d73e49ba6
Badrakova Valeria <lera_18a@mail.ru>
2026-02-05 19:46:25 +0300

    feat(mock_api): update products and images

--- mock_api/server.js
+++ mock_api/server.js
@@ -8,20 +8,20 @@ app.use(cors());
 app.use(express.json());
 
 const productsByBarcode = {
-    "4601234567890": {
-        barcode: "4601234567890",
+    "4601234567893": {
+        barcode: "4601234567893",
         name: "Кроссовки Nike Air",
-        imageUrl: "http://localhost:3000/images/4601234567890.jpg",
+        imagePath: "/images/4601234567893.jpg",
         sizes: [
             { size: "40", inStock: true, qty: 2 },
             { size: "41", inStock: false, qty: 0 },
             { size: "42", inStock: true, qty: 5 }
         ]
     },
-    "123456789012": {
-        barcode: "123456789012",
+    "1234567890128": {
+        barcode: "1234567890128",
         name: "Футболка Basic",
-        imageUrl: "http://localhost:3000/images/123456789012.jpg",
+        imagePath: "/images/1234567890128.jpg",
         sizes: [
             { size: "S", inStock: true, qty: 10 },
             { size: "M", inStock: false, qty: 0 },
@@ -30,20 +30,26 @@ const productsByBarcode = {
     }
 };
 
-const hostname = 'localhost';
-app.use('/images', express.static(path.join(__dirname, "assets/images")));
-//если не работает снеси эту хуйню
-app.get("/health", (_, res) => res.json({ ok: true }));
+function imageUrl(req, p) {
+    const host = req.get('host');
+    const protocol = 'http';
+    return {
+        ...p, imageUrl:
+            `${protocol}://${host}${p.imagePath}`
+    }
+}
 
+app.use('/images', express.static(path.join(__dirname, "assets/images")));
+
+app.get("/health", (_, res) => res.json({ ok: true }));
 
 app.get("/api/products/:barcode", (req, res) => {
     const p = productsByBarcode[req.params.barcode];
     if (!p) return res.status(404).json({ error: "NOT_FOUND" });
-    return res.json(p);
+    return res.json(imageUrl(req, p));
 });
 
-// 0.0.0.0 важно для реального iPhone позже
 const port = 3000;
 app.listen(port, "0.0.0.0", () => {
-    console.log(`Mock API running on http://${hostname}:${port}`);
+    console.log(`Mock API running on port ${port}`);
 });
